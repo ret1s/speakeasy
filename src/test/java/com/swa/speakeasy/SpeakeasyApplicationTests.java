f1aed3263e78ba2ded924dc84e4ce36407be1044
geohanz <ngmhah@gmail.com>
2024-02-22 16:24:56 +0700

    project intialization

---
+++ src/test/java/com/swa/speakeasy/SpeakeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swa.speakeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
