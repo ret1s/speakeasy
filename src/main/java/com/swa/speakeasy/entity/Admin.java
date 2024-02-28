7073151f920342c1b8c5316ee81b373ebe5a55cc
geohanz <ngmhah@gmail.com>
2024-02-28 19:53:49 +0700

    add admin entity

---
+++ src/main/java/com/swa/speakeasy/entity/Admin.java
@@ -0,0 +1,39 @@
+package com.swa.speakeasy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phoneNumber;
+
+}
